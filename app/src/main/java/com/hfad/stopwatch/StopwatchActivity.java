c0d85ff5fad1c7f8decbe373ea1b97658ad564af
damarendra <damarendran@gmail.com>
2021-02-03 15:25:54 +0700

    Finished Chapter 3

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -3,12 +3,103 @@ package com.hfad.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class StopwatchActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running = false;
+    private boolean wasRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+
+        if(savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+
+        runTimer();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        if(wasRunning) running = true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(wasRunning) running = true;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        wasRunning = running;
+//        running = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void runTimer() {
+        final TextView mTimeView = findViewById(R.id.time_view);
+        final Handler mHandler = new Handler(Looper.getMainLooper());
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+
+                mTimeView.setText(
+                        String.format(Locale.getDefault(),
+                    "%d:%02d:%02d", hours, minutes, secs));
+                if(running) seconds++;
+
+                mHandler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    public void onStartPressed(View view) {
+        running = true;
+    }
+
+    public void onStopPressed(View view) {
+        running = false;
+    }
+
+    public void onResetPressed(View view) {
+        running = false;
+        seconds = 0;
     }
 }
